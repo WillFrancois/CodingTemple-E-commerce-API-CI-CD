1aa8f0905ba03abd6da2aa2a4040662ee7600e4d
WillFrancois <williamjcfrancois@gmail.com>
2025-01-08 20:09:42 -0500

    Added files and modified config to allow for Render deployment

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:CodingTemple@localhost/advanced_e_commerce_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://root:8Fmge6V8cJnUwWTPC5lmTg5Z9e2z5jc6@dpg-ctvhnnaj1k6c73dmddlg-a.ohio-postgres.render.com/advanced_e_commerce_db_vmi0'
