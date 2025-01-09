198a9398e0a5eb9cace72c5ed6eb3e97142f799c
WillFrancois <williamjcfrancois@gmail.com>
2025-01-08 20:34:00 -0500

    Added comment for clarity of what config was being added from previous iteration

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 class DevelopmentConfig:
+    #Updated for postgresql
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://root:8Fmge6V8cJnUwWTPC5lmTg5Z9e2z5jc6@dpg-ctvhnnaj1k6c73dmddlg-a.ohio-postgres.render.com/advanced_e_commerce_db_vmi0'
